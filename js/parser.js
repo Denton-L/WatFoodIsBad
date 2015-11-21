f281281d9629b4087d4f71941132ac05f9406879
Denton-L <liu.denton+github@gmail.com>
2015-11-20 21:53:05 -0500

    Added parser logic. Please god, let this work on the first try

--- js/parser.js
+++ js/parser.js
@@ -1,11 +1,70 @@
+var locationsDict = {};
+var inspectionsDict = {};
+var notifyLocationsDone;
+var notifyInspectionsDone;
+var notifyInfractionsDone;
+
 parseLocations(file) {
-		// I don't know
+		Papa.parse(file, {
+				header: true,
+				worker: true,
+				chunk: locationChunk,
+				complete: function(results, file) {
+						notifyLocationsDone();
+				}
+		});
 }
 
 parseInspections(file) {
-		// stuff
+		Papa.parse(file, {
+				header: true,
+				worker: true,
+				chunk: inspectionChunk,
+				complete: function(results, file) {
+						notifyInspectionsDone();
+				}
+		});
 }
 
 parseInfractions(file) {
-		// stuff
+		Papa.parse(file, {
+				header: true,
+				worker: true,
+				chunk: infractionChunk,
+				complete: function(results, file) {
+						delete inspectionsDict;
+						notifyInfractionDone();
+		});
+}
+
+locationChunk(results, parser) {
+		for (var i = 0; i < results.length; i++) {
+				locationsDict[results[i].FACILITYID] = {
+						name: results[i].BUSINESS_NAME,
+						address: results[i].ADDR,
+						city: results[i].CITY,
+						numCritical: 0,
+						numNonCritical: 0,
+						inspections: []
+				};
+		}
+}
+
+inspectionChunk(results, parser) {
+		for (var i = 0; i < results.length; i++) {
+				locationsDict[results[i].FACILITYID].inspections.push(inspectionsDict[results[i].INSPECTION_ID] = {
+						date: INSPECTION_DATE,
+						requireInspection: REQUIRE_INSPECTION,
+						infractions: []
+				});
+		}
+}
+
+infractionChunk(results, parser) {
+		for (var i = 0; i < results.length; i++) {
+				inspectionsDict[results[i].INSPECTION_ID].infractions.push({
+						description: category_code,
+						infractionType: INFRACTION_TYPE
+				});
+		}
 }
