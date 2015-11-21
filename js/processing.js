03db91fd3654f69e9bf237d46843dc6b233428ac
Matt D'Souza <matt.dsouza@gmail.com>
2015-11-21 00:21:59 -0500

    apparently for loops suck in js

--- js/processing.js
+++ js/processing.js
@@ -1,18 +1,21 @@
 function rank(locations) {
-	for (location in locations) {
+	for (var i = 0; i < locations.length; i++) {
 		rank = 0
-		for (inspection in location['inspections']) {
+		var location = locations[i];
+		for (var j = 0; j < location['inspections'].length; j++) {
+			var inspection = location['inspections'][j];
 			dateInMillis = new Date(date).getTime();
 			now = new Date().getTime();
 			numYears = (now-dateInMillis)/(365*24*60*60*1000);
-			for (infraction in inspection['infractions']) {
+			for (var k = 0; k < inspection['infractions'].length; k++) {
+				var infraction = inspection['infractions'][k];
 				if (infraction['infractionType'] == 'CRITICAL') {
 					rank += Math.pow(0.8,numYears);
 				}
 				else if (infraction['infractionType'] == 'NON-CRITICAL') {
 					rank += 0.5*Math.pow(0.8,numYears);
 				}
-			}
+			}	
 		}
 		location['rank'] = rank/location['inspections'].length;
 	}
@@ -22,7 +25,8 @@ function rank(locations) {
 
 function getTopTen(locations) {
 	top = [];
-	for (location in locations) {
+	for (var j = 0; j < locations.length; j++) {
+		var location = locations[j];
 		rank = location['rank'];
 		var i = 0;
 		while(i < 10 ) {
