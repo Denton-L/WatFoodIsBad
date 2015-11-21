83270c7a2d3e4924faef12bbd54c0a2504e8bfc4
Matt D'Souza <matt.dsouza@gmail.com>
2015-11-21 00:09:50 -0500

    topten

--- js/processing.js
+++ js/processing.js
@@ -16,5 +16,22 @@ function rank(locations) {
 		}
 		location['rank'] = rank/location['inspections'].length;
 	}
-
 }
+
+
+
+function getTopTen(locations) {
+	top = [];
+	for (location in locations) {
+		rank = location['rank'];
+		var i = 0;
+		while(i < 10 ) {
+			if (rank > top[i]['rank']){
+				top.splice(i, 0, location);
+				break;
+			}
+			i += 1;
+		}
+	}
+	return top;
+} 
